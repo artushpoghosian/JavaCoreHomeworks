23fa5aa9cdfac00cc83fde3f0610a2adc492404b
Artush Poghosyan <artushpoghosian@gmail.com>
2025-09-11 18:06:29 +0400

    Homework 01

--- src/Homework01/FigurePainter.java
+++ src/Homework01/FigurePainter.java
@@ -4,8 +4,8 @@ public class FigurePainter {
     public static void main(String[] args) {
 
         System.out.println("TASK 1");
-        for (int i = 0; i < 5; i++){
-            for (int j = 0; j < i; j++){
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
             System.out.println("*");
@@ -14,8 +14,8 @@ public class FigurePainter {
         System.out.println();
 
         System.out.println("TASK 2");
-        for (int i = 0; i < 5; i++){
-            for (int j = i; j < 4; j++){
+        for (int i = 0; i < 5; i++) {
+            for (int j = i; j < 4; j++) {
                 System.out.print("*");
             }
             System.out.println("*");
@@ -24,8 +24,8 @@ public class FigurePainter {
         System.out.println();
 
         System.out.println("TASK 3");
-        for (int i = 0; i < 5; i++){
-            for (int j = i; j < 4; j++){
+        for (int i = 0; i < 5; i++) {
+            for (int j = i; j < 4; j++) {
                 System.out.print(" ");
             }
             for (int k = 0; k < i; k++) {
@@ -37,8 +37,8 @@ public class FigurePainter {
         System.out.println();
 
         System.out.println("TASK 4");
-        for (int i = 0; i < 5; i++){
-            for (int j = 0; j < i; j++){
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print(" ");
             }
             for (int k = i; k < 4; k++) {
@@ -51,8 +51,8 @@ public class FigurePainter {
 
         System.out.println("TASK 5");
 
-        for (int i = 0; i < 5; i++){
-            for (int j = i; j < 4; j++){
+        for (int i = 0; i < 5; i++) {
+            for (int j = i; j < 4; j++) {
                 System.out.print(" ");
             }
             for (int k = 0; k < i; k++) {
@@ -60,8 +60,8 @@ public class FigurePainter {
             }
             System.out.println("*");
         }
-        for (int i = 1; i < 5; i++){
-            for (int j = 0; j < i; j++){
+        for (int i = 1; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print(" ");
             }
             for (int k = i; k < 4; k++) {
@@ -79,8 +79,8 @@ public class FigurePainter {
          */
         System.out.println("Optimized version of TASK 5");
 
-        for (int i = 0; i < 9; i++){
-            for (int j = 0; j < Math.abs(4 - i); j++){
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < Math.abs(4 - i); j++) {
                 System.out.print(" ");
             }
             for (int k = 0; k < 5 - Math.abs(4 - i); k++) {
